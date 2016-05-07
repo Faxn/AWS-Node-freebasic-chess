9b4d32c84485507a55271880f7c7ce25ed40b2b3
pjohn7 <pjohn1996@gmail.com>
2016-05-07 19:28:21 -0400

    added basic play template

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var http = require('http'),
     pieces = require('./pieces.js'),
     board = require('./board.js')
 
-board = new board.mailbox();
+
     
 // configure the app
 var app = express();
@@ -14,14 +14,32 @@ var app = express();
 //host static files.
 app.use(serveStatic('static/'));
 
+//setup the global board
+board = new board.mailbox();
 
-
+//serve the index
 indexhb = fs.readFileSync('templates/index.html', "utf-8")
 indextem = handlebars.compile(indexhb)
 app.get("/", function(req, res){
     html = indextem({motd:"handlebars"})
     res.send(html)
 })
+
+//serve the play pages
+playhb =fs.readFileSync('templates/simple_play.html', 'utf-8')
+playtem = handlebars.compile(playhb) 
+
+app.get("/play/1/white", function(req, res){
+	html=playtem(board)	
+	res.send(html)
+})
+
+app.get("/play/1/black", function(req, res){
+	res.send("The Abyss watchers")
+})
+
+
+app.post("/play/1/move",function(req, res){}) 
 
 //host the server
 app.set('port', 8094);
