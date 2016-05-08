4c896c457b3c6dfcbe81d14430c469236e013f07
sternjar <sternjar@kean.edu>
2016-05-08 03:13:47 -0400

    Commit

--- verify.js
+++ verify.js
@@ -1,71 +1,90 @@
 var board = require("./board.js");
 function Verify(Player, SC1, SC2, mailbox){
-    vOne = (mailbox[SC1] / 128);
-    vTwo = (mailbox[SC2] / 128);
+   
+    vOne = Math.floor(mailbox.arr[SC1] / 128);
+    vTwo = Math.floor(mailbox.arr[SC2] / 128);
     xorOne = Boolean(Player == vOne);
     xorTwo = Boolean(Player == vTwo);
-    
-    if ((xorOne && !xorTwo) || (!xorOne && !xorTwo))
+ 
+    type = 0;
+    origin = 0;
+    destination = 0;
+
+    if ((xorOne && !xorTwo) || (!xorOne && xorTwo))
     {
+  
     if (Player == vOne) 
     {
-        type = (mailbox[SC1] % 128);
+         
+        type = (mailbox.arr[SC1] % 128);
+       
         origin = SC1;
         destination = SC2;
         
+       
+       return validMove(type, origin, destination, Player, mailbox);
     
        
     }
     else if (Player == vTwo)
     {
-        type = (mailbox[SC2] % 128);
+        type = (mailbox.arr[SC2] % 128);
         origin = SC2;
         destination = SC1;
-     
+        
+    return validMove(type, origin, destination, Player, mailbox);
     }
-    validMove(type, origin, destination, Player, mailbox);
    
     
     }
     else 
     {
-    return "Invalid Move.";
+    return false;
     }
-    
-}
+ 
+        
 
-function validMove(ptype, orig, dest, player, mailbox){
-    if (orig > dest){
-         min = dest;
-         max = orig;
+}
+function validMove(type, origin, destination, Player, mailbox){
+    
+    if (origin > destination){
+         min = destination;
+         max = origin;
+         
     }
     else {
-        min = orig;
-        max = dest;
+        
+        
+        min = origin;
+        max = destination;
     }
     travel = max - min;
-    if (ptype == 1){
+
+    if (type == 1){
         if (travel == 16){
-           if (((orig <= 7)) || ((orig >= 16) && (orig <= 47)) || (orig >= 56)){
+            
+           if (((origin <= 7)) || ((origin >= 16) && (origin <= 47)) || (origin >= 56)){
                return false
+               
            }
            else {
                increment = 16;
+               return commitMove(type, origin, destination, Player, mailbox);
            }
            
            }
           else if ((travel == 8)){
-              if (mailbox[dest] != 0){
+              if (mailbox.arr[destination] != 0){
                   return false
               }
               else {increment = 8;}
           }
           else if ((travel == 7) || (travel == 9)){
-              if (mailbox[dest] == 0){
+              if (mailbox.arr[destination] == 0){
                   return false;
               }
               else {
-              commitMove(ptype, origin, dest, player, mailbox);
+            return commitMove(type, origin, destination, Player, mailbox);
               }
           }
         else {
@@ -76,15 +95,15 @@ function validMove(ptype, orig, dest, player, mailbox){
         
         
     }
-    else if (ptype == 2){
+    else if (type == 2){
         if ((travel == 15) || (travel == 17) || (travel == 6) || (travel ==10)){
-            commitMove(ptype, origin, dest, player, mailbox);
+           return commitMove(type, origin, destination, Player, mailbox);
         }
         else {
-            return "Invalid Move.";
+            return false;
         }
     }
-    else if (ptype == 3){
+    else if (type == 3){
         if ((travel % 9 == 0))
         {
             increment = 9;
@@ -94,11 +113,11 @@ function validMove(ptype, orig, dest, player, mailbox){
         }
         else 
         {
-            return "Invalid Move.";
+            return false;
         }
         
     }
-    else if (ptype == 4){
+    else if (type == 4){
         if (travel % 8 == 0){
             increment = 8;
         }
@@ -107,7 +126,7 @@ function validMove(ptype, orig, dest, player, mailbox){
              j = 7 + i;
              if ((min >= i && min <= j) && (max >= i && max <= j)){
                  increment = 1;
-                 travelTrace(min, max, increment, player, mailbox, dest, origin, ptype);
+                 travelTrace(min, max, increment, Player, mailbox, destination, origin, type);
              }
          }
          
@@ -117,16 +136,22 @@ function validMove(ptype, orig, dest, player, mailbox){
         }
         
     }
-    else if (ptype == 5){
+    else if (type == 5){
+        
          if ((travel == 1) || (travel == 8) || (travel == 7) || (travel == 9)){
-            commitMove(ptype, origin, dest, player, mailbox);
+            
+            commitMove(type, origin, destination, Player, mailbox);
+            
         }
         else {
-            return "Invalid Move.";
+       
+            return false;
+        
         }
         
     }
-    else if (ptype == 6){
+    else if (type == 6){
+        
               if (travel % 8 == 0){
             increment = 8;
         }
@@ -142,7 +167,7 @@ function validMove(ptype, orig, dest, player, mailbox){
              j = 7 + i;
              if ((min >= i && min <= j) && (max >= i && max <= j)){
                  increment = 1;
-                 travelTrace(min, max, increment, player, mailbox, dest, origin, ptype);
+                return travelTrace(min, max, increment, Player, mailbox, destination, origin, type);
              }
              else {
                 return false;
@@ -151,42 +176,34 @@ function validMove(ptype, orig, dest, player, mailbox){
         }
         }
         
-    travelTrace(min, max, increment, player, mailbox, dest, origin, ptype);
+   return travelTrace(min, max, increment, Player, mailbox, destination, origin, type);
     
 
-    function travelTrace(min, max, increment, player, mailbox, dest, origin, ptype){
+    function travelTrace(min, max, increment, Player, mailbox, destination, origin, type){
         min = min + increment;
         while (min < max){
-            if (mailbox[min] != 0){
-                return "Invalid Move";
+            if (mailbox.arr[min] != 0){
+                return false;
             }
             min = min + increment;
         }
-        commitMove(ptype, origin, dest, player, mailbox);
+       return commitMove(type, origin, destination, Player, mailbox);
     }
     
    
-    function commitMove(ptype, orig, dest, player, mailbox){
-        if (player == 1){
-            ptype = ptype + 128;
+    function commitMove(type, orig, destination, Player, mailbox){
+                console.log("Write Success");
+
+        if (Player == 1){
+            type = type + 128;
         }
-        if ((mailbox[dest] == 5) || (mailbox[dest] == 133)){
-            if (player == 0){
-                player == "White";
-            }
-            else {
-                player == "Black";
-            }
-            }
-            winner = player;
-            gameover = 1;
-        }
-        mailbox[dest] = ptype;
-        mailbox[orig] = 0;
-        if (gameover == 1){
-            return player +" Player is the winner!";
+     
+        mailbox.arr[destination] = type;
+        mailbox.arr[origin] = 0;
+        console.log("Write Success");
+        return true;
         }
    }
     
-    }
+}
 exports.verify = Verify;
